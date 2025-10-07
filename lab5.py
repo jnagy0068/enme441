fc9a0f3a39695d40db443ab957c830339139c791
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-10-07 15:15:49 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
+import math
 GPIO.setmode(GPIO.BCM)
 
-p = 25    # GPIO pin number
-f = 1     # frequency (Hz)
+pins = [5, 6, 12, 13, 16, 19, 20, 21, 25, 26]    # GPIO pin number
+f = 500     # frequency (Hz)
 
-GPIO.setup(p, GPIO.OUT)
-pwm = GPIO.PWM(p, f)        # create PWM object
+for x in pins:
+  GPIO.setup(x, GPIO.OUT)
+  pwm = GPIO.PWM(x, f)        # create PWM object
 
 try:
-  pwm.start(dc)             # initiate PWM object
   while True:
+    pwm.start((sin(2 * math.pi * f * time.time()))^2)            # initiate PWM object
     pass
 except KeyboardInterrupt:   # stop gracefully on ctrl-C
   print('\nExiting')
