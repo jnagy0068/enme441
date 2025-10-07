4d984950b122e2f7845f6ca0e57fc1df2fd6766d
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-10-07 16:02:03 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -12,11 +12,11 @@ for x in pins:
   pwm[x] = GPIO.PWM(x, f)        # create PWM object
 
 try:
-  for y in pins:
+  while True:
+    for y in pins:
     i = 0
     pwm[y].start(((math.sin(2 * math.pi * f * time.time() - i*math.pi/9))**2)*100) 
     i += 1
-  while True:
     pass
 except KeyboardInterrupt:   # stop gracefully on ctrl-C
   print('\nExiting')
