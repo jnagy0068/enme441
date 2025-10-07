b03628a7577382078714afc64c59c213e50acfbd
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-10-07 16:25:00 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -4,13 +4,25 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 pins = [5, 6, 12, 13, 16, 19, 20, 21, 25, 26]  
+button_pin = 4
 pwm = {}
 f = 500  
+direction = 1       # 1 for forward, -1 for reverse
 
 for x in pins:
   GPIO.setup(x, GPIO.OUT)
   pwm[x] = GPIO.PWM(x, f)  
   pwm[x].start(0)
+
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# --- Callback for button press ---
+def toggle_direction(channel):
+    global direction
+    direction *= -1
+    print(f"Direction changed to {'forward' if direction == 1 else 'reverse'}")
+
+GPIO.add_event_detect(button_pin, GPIO.RISING, callback=toggle_direction, bouncetime=300)
 
 try:
   while True:
