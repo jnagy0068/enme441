c579dac9005827c77bf43da6671342871ae66780
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-10-07 16:02:53 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -14,9 +14,9 @@ for x in pins:
 try:
   while True:
     for y in pins:
-    i = 0
-    pwm[y].start(((math.sin(2 * math.pi * f * time.time() - i*math.pi/9))**2)*100) 
-    i += 1
+      i = 0
+      pwm[y].start(((math.sin(2 * math.pi * f * time.time() - i*math.pi/9))**2)*100) 
+      i += 1
     pass
 except KeyboardInterrupt:   # stop gracefully on ctrl-C
   print('\nExiting')
