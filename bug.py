1fa1aff9d8b0ac7eadf074c3afeda713601b2c76
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-10-13 19:16:07 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,27 +1,32 @@
-import RPi.GPIO as GPIO
 import time
+import random
+import RPi.GPIO as GPIO
+from shifter import Shifter
 
-GPIO.setmode(GPIO.BCM)
+dataPin = 23
+latchPin = 24
+clockPin = 25
 
-class Shifter:
-    def __init__(self, dataPin, latchPin, clockPin):
-        self.dataPin = dataPin
-        self.latchPin = latchPin
-        self.clockPin = clockPin
+leds = Shifter(dataPin, latchPin, clockPin)
 
-        # Set up GPIO pins
-        GPIO.setup(self.dataPin, GPIO.OUT)
-        GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
-        GPIO.setup(self.clockPin, GPIO.OUT, initial=0)
+position = 3  # start roughly in the middle
+dt = 0.05  # 50 ms between updates
 
-    def _ping(self, pin):
-        GPIO.output(pin, 1)
-        time.sleep(0.00001)
-        GPIO.output(pin, 0)
-        time.sleep(0.00001)
+try:
+    while True:
+        pattern = 1 << position
+        leds.shiftByte(pattern)
 
-    def shiftByte(self, pattern):
-        for i in range(8):
-            GPIO.output(self.dataPin, (pattern >> i) & 1)
-            self._ping(self.clockPin)
-        self._ping(self.latchPin)
+        step = random.choice([-1, 1])
+        position += step
+
+        if position < 0:
+            position = 0
+        elif position > 7:
+            position = 7
+
+        time.sleep(dt)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("\nProgram stopped and GPIO cleaned up.")
