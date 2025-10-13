ea6b836d09d3eff40332ede222ba418e7a4f144a
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-10-13 18:24:00 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,1 +1,30 @@
+import time
+import random
+import RPi.GPIO as GPIO
+from shifter import Shifter
+
+dataPin, latchPin, clockPin = 23, 24, 25
+led_shifter = Shifter(dataPin, clockPin, latchPin)
+
+position = 3 
+dt = 0.05
+
+try:
+    while True:
+        pattern = 1 << position
+        led_shifter.shiftByte(pattern)
+
+        step = random.choice([-1, 1])
+        position += step
+
+        if position < 0:
+            position = 0
+        elif position > 7:
+            position = 7
+
+        time.sleep(dt)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("\nRandom walk stopped and GPIO cleaned up.")
 
