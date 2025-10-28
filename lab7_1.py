7aec4a668c288ee4c768efe1c5668c3f06f5eac1
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-10-28 19:51:36 -0400

    Update lab7_1.py

--- lab7_1.py
+++ lab7_1.py
@@ -36,17 +36,19 @@ def parsePOSTdata(data):
             data_dict[key_val[0]] = key_val[1]
     return data_dict
 
-def web_page():
+def web_page(selected_led=0):
+    c0 = 'checked' if selected_led == 0 else ''
+    c1 = 'checked' if selected_led == 1 else ''
+    c2 = 'checked' if selected_led == 2 else ''
     html = """
         <html>
         <form action="/cgi-bin/range.py" method="POST">
             Brightness Level:<br>
-            <input type="range" name="brightness" min ="0" max="100" value ="0"/> <br>
-            <br>
+            <input type="range" name="brightness" min="0" max="100" value="{brightness[selected_led]}"> {brightness[selected_led]}%<br><br>
             Select LED:<br>
-            <input type="radio" name="led1" value="led1"> LED 1 <br>
-            <input type="radio" name="led2" value="led2"> LED 2 <br>
-            <input type="radio" name="led3" value="led3"> LED 3 <br>
+            <input type="radio" name="led" value="0" {c0}> LED 1 ({brightness[0]}%)<br>
+            <input type="radio" name="led" value="1" {c1}> LED 2 ({brightness[1]}%)<br>
+            <input type="radio" name="led" value="2" {c2}> LED 3 ({brightness[2]}%)<br>
             <br>
             <input type="submit" value="Change Brightness">
         </form>
