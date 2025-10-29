784303e46f12f42efff1ad01f986f902bd6dee91
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-10-29 13:22:47 -0400

    Update lab7_1.py

--- lab7_1.py
+++ lab7_1.py
@@ -3,9 +3,9 @@ import threading
 import RPi.GPIO as GPIO
 import time
 
-led_pins = [17, 27, 22]  
-f = 1000           
-brightness = [0, 0, 0]    
+led_pins = [17, 27, 22]
+f = 1000
+brightness = [0, 0, 0]
 pwms = []
 
 GPIO.setmode(GPIO.BCM)
@@ -16,19 +16,15 @@ for pin in led_pins:
     pwms.append(pwm)
 
 def change_brightness(index, value):
-    value = int(value)
-    if value < 0:
-        value = 0
-    if value > 100:
-        value = 100
+    value = max(0, min(100, int(value)))
     brightness[index] = value
     pwms[index].ChangeDutyCycle(value)
 
 def parsePOSTdata(data):
     data_dict = {}
-    idx = data.find('\r\n\r\n')+4
-    data = data[idx:]
-    data_pairs = data.split('&')
+    idx = data.find('\r\n\r\n') + 4
+    post_data = data[idx:]
+    data_pairs = post_data.split('&')
     for pair in data_pairs:
         key_val = pair.split('=')
         if len(key_val) == 2:
@@ -39,48 +35,57 @@ def web_page(selected_led=0):
     c0 = 'checked' if selected_led == 0 else ''
     c1 = 'checked' if selected_led == 1 else ''
     c2 = 'checked' if selected_led == 2 else ''
-    html = """
-        <html>
-        <form action="/cgi-bin/range.py" method="POST">
-            Brightness Level:<br>
-            <input type="range" name="brightness" min="0" max="100" value="{brightness[selected_led]}"> {brightness[selected_led]}%<br><br>
-            Select LED:<br>
+    html = f"""
+    <html>
+    <head><title>LED Control</title></head>
+    <body style="font-family: Arial; text-align: center; margin-top: 50px;">
+        <h2>LED Brightness Control</h2>
+        <form action="/" method="POST">
+            <label>Brightness Level:</label><br>
+            <input type="range" name="brightness" min="0" max="100" value="{brightness[selected_led]}" oninput="this.nextElementSibling.value=this.value">
+            <output>{brightness[selected_led]}</output>%<br><br>
+
+            <label>Select LED:</label><br>
             <input type="radio" name="led" value="0" {c0}> LED 1 ({brightness[0]}%)<br>
             <input type="radio" name="led" value="1" {c1}> LED 2 ({brightness[1]}%)<br>
-            <input type="radio" name="led" value="2" {c2}> LED 3 ({brightness[2]}%)<br>
-            <br>
+            <input type="radio" name="led" value="2" {c2}> LED 3 ({brightness[2]}%)<br><br>
+
             <input type="submit" value="Change Brightness">
         </form>
-        </html>
-        """
-    print(html)
-    return (bytes(html,'utf-8'))
-     
+    </body>
+    </html>
+    """
+    return bytes(html, 'utf-8')
+
 def serve_web_page():
     while True:
         print("Waiting for connection...")
         conn, (client_ip, client_port) = s.accept()
         print(f"Connection from {client_ip}:{client_port}")
-        client_message = conn.recv(2048).decode('utf-8')
-print(f"Message from client:\n{client_message}")
+        
+        client_message = conn.recv(2048).decode('utf-8', errors='ignore')
+        print(f"Message from client:\n{client_message}")
 
-if client_message.startswith('POST'):
-    data_dict = parsePOSTdata(client_message)
-else:
-    data_dict = {}
+        if client_message.startswith('POST'):
+            data_dict = parsePOSTdata(client_message)
+        else:
+            data_dict = {}
 
-if 'led' in data_dict and 'brightness' in data_dict:
-    led = int(data_dict['led'])
-    value = int(data_dict['brightness'])
-    change_brightness(led, value)
-    conn.send(b'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n')
-    conn.sendall(web_page(led))
-else:
-    conn.send(b'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n')
-    conn.sendall(web_page())
+        if 'led' in data_dict and 'brightness' in data_dict:
+            led = int(data_dict['led'])
+            value = int(data_dict['brightness'])
+            change_brightness(led, value)
+            response = web_page(led)
+        else:
+            response = web_page()
 
+        conn.send(b'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n')
+        conn.sendall(response)
+        conn.close()
+
+# --- Server setup ---
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
+s.bind(('', 8080))   # use 80 if running with sudo
 s.listen(3)
 
 webpageThread = threading.Thread(target=serve_web_page, daemon=True)
@@ -91,7 +96,7 @@ try:
         time.sleep(1)
 except KeyboardInterrupt:
     print('\nExiting')
-    for pwm in pwms: 
+    for pwm in pwms:
         pwm.stop()
     GPIO.cleanup()
     s.close()
