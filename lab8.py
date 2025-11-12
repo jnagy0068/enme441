8218b7b150f8bc0ac33270ccdd66fe42832d0c4b
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-11-12 14:43:47 -0500

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -1,46 +1,45 @@
 import time
-import sys
 import multiprocessing
-import RPi.GPIO as GPIO
-from shifter import Shifter
+from shifter import Shifter  # your custom module
 
-shared_states = multiprocessing.Array('i', 2)
+# Shared array for two steppers
+myArray = multiprocessing.Array('i', 2)
 
 class Stepper:
-    seq = [0b0001, 0b0011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001]
-
-    base_delay = 3000 #effects motor turn speed, too low can cause stalling
+    seq = [0b0001, 0b0011, 0b0010, 0b0110,
+           0b0100, 0b1100, 0b1000, 0b1001]
+    delay = 2000  # microseconds (adjust for speed)
     steps_per_degree = 1024 / 360
 
     def __init__(self, shifter, lock, index):
         self.s = shifter
         self.lock = lock
-        self.index = index 
-        self.angle = multiprocessing.Value('d', 0.0)
+        self.index = index
+        self.angle = 0
         self.step_state = 0
-        self.bit_offset = 4 * index
+        self.shifter_bit_start = 4 * index
 
     def _sgn(self, x):
         return 0 if x == 0 else int(abs(x) / x)
 
     def _step(self, direction):
-        self.step_state = (self.step_state + direction) % 8
-        pattern = Stepper.seq[self.step_state] << self.bit_offset
-
         with self.lock:
-            mask = 0b1111 << self.bit_offset
-            shared_states[self.index] = (shared_states[self.index] & ~mask) | pattern
+            self.step_state = (self.step_state + direction) % 8
 
-            combined = 0
-            for v in shared_states:
-                combined |= v
+            # Clear and set only this motor's 4 bits
+            myArray[self.index] &= ~(0b1111 << self.shifter_bit_start)
+            myArray[self.index] |= (Stepper.seq[self.step_state] << self.shifter_bit_start)
 
-            self.s.shiftByte(combined)
+            # Combine both motors' patterns
+            final = 0
+            for val in myArray:
+                final |= val
 
-        with self.angle.get_lock():
-            self.angle.value = (self.angle.value + direction / Stepper.steps_per_degree) % 360
+            self.s.shiftByte(final)
 
-        time.sleep(Stepper.base_delay / 1e6)
+        # Update local angle
+        self.angle = (self.angle + direction / Stepper.steps_per_degree) % 360
+        time.sleep(Stepper.delay / 1e6)
 
     def _rotate(self, delta):
         direction = self._sgn(delta)
@@ -54,47 +53,34 @@ class Stepper:
         return p
 
     def goAngle(self, target):
-        with self.angle.get_lock():
-            current = self.angle.value
-
-        delta = (target - current + 540) % 360 - 180
+        # Use shortest angular distance
+        delta = (target - self.angle + 540) % 360 - 180
         return self.rotate(delta)
 
     def zero(self):
-        with self.angle.get_lock():
-            self.angle.value = 0.0
-
+        self.angle = 0
 
 if __name__ == '__main__':
-    try:
-        s = Shifter(23, 24, 25)
-        lock1 = multiprocessing.Lock()
-        lock2 = multiprocessing.Lock()
+    s = Shifter(23, 24, 25)
+    lock1 = multiprocessing.Lock()
+    lock2 = multiprocessing.Lock()
 
-        m1 = Stepper(s, lock1, 0)
-        m2 = Stepper(s, lock2, 1)
+    m1 = Stepper(s, lock1, 0)
+    m2 = Stepper(s, lock2, 1)
 
-        m1.zero()
-        m2.zero()
+    m1.zero()
+    m2.zero()
 
-        p1 = m1.goAngle(90)
-        p2 = m2.goAngle(-90)
-        p1.join()
-        p2.join()
+    p1 = m1.goAngle(90)
+    p2 = m2.goAngle(-90)
+    p1.join(); p2.join()
 
-        p1 = m1.goAngle(-45)
-        p1.join()
+    p1 = m1.goAngle(-45)
+    p1.join()
 
-        p1 = m1.goAngle(135)
-        p2 = m2.goAngle(45)
-        p1.join()
-        p2.join()
+    p1 = m1.goAngle(135)
+    p2 = m2.goAngle(45)
+    p1.join(); p2.join()
 
-        p1 = m1.goAngle(0)
-        p1.join()
-
-    except KeyboardInterrupt:
-        print("\n keyboard interrupt")
-    finally:
-        GPIO.cleanup()
-        sys.exit(0)
+    p1 = m1.goAngle(0)
+    p1.join()
