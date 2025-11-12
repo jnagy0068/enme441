2d0dabd9508905d549e9e1b9bb37b0aa2233ef6f
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-11-12 16:35:45 -0500

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -5,8 +5,7 @@ from shifter import Shifter
 myArray = multiprocessing.Array('i', 2)
 
 class Stepper:
-    seq = [0b0001, 0b0011, 0b0010, 0b0110,
-           0b0100, 0b1100, 0b1000, 0b1001]
+    seq = [0b0001, 0b0011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001]
     delay = 2000 
     steps_per_degree = 1024 / 360
 
