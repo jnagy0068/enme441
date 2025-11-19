dcc6a6a44219dc51e3e8e8476180ebbc1d7f937a
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2025-11-19 16:00:36 -0500

    Update project.py

--- project.py
+++ project.py
@@ -14,7 +14,7 @@ myArray = multiprocessing.Array('i', 2)
 
 class Stepper:
     seq = [0b0001, 0b0011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001]
-    delay = 2000 
+    delay = 3000 
     steps_per_degree = 1024 / 360
 
     def __init__(self, shifter, lock, index):
